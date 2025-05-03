dd22d914e4f1c20cc13d8118ab6219f896950383
jadrienoofy <ddarkknife@gmail.com>
2025-05-03 13:16:22 +0800

    Add files via upload

---
+++ feedback.js
@@ -0,0 +1,4 @@
+    function toggleFeedbackForm() {
+        const form = document.getElementById('feedbackForm');
+        form.style.display = form.style.display === 'block' ? 'none' : 'block';
+    }
